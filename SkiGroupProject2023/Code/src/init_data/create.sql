6f39a9fc31693985b1c6a4ebf5d70cc2a7db3add
ParkerrAllen <89681695+ParkerrAllen@users.noreply.github.com>
2025-03-30 07:21:32 -0600

    Add files via upload

---
+++ SkiGroupProject2023/Code/src/init_data/create.sql
@@ -0,0 +1,54 @@
+CREATE TABLE users (
+    user_id SERIAL PRIMARY KEY,
+    username VARCHAR(50) NOT NULL,
+    email VARCHAR(200) NOT NULL,
+    password CHAR(60) NOT NULL
+);
+
+CREATE TABLE user_to_trips(
+    user_id SMALLINT,
+    trip_id SMALLINT
+);
+
+CREATE TABLE past_trips(
+    past_trip_id SERIAL PRIMARY KEY,
+    trip_id SMALLINT,
+    link VARCHAR(200) NOT NULL,
+    location VARCHAR(50) NOT NULL,
+    duration INTEGER NOT NULL
+);
+
+CREATE TABLE trips(
+ trip_id SERIAL PRIMARY KEY,
+ trip_name VARCHAR(50) NOT NULL,
+ country VARCHAR(50) NOT NULL,
+ state_ VARCHAR(50) NOT NULL,
+ summit_ SMALLINT,
+ base SMALLINT,
+ verticle SMALLINT,
+ lifts SMALLINT,
+ runs SMALLINT,
+ lat DECIMAL NOT NULL,
+ lon DECIMAL NOT NULL,
+ trip_picture VARCHAR(500) NOT NULL,
+ link VARCHAR(500) NOT NULL
+);
+
+CREATE TABLE user_to_products(
+    user_id SMALLINT,
+    product_id SMALLINT
+);
+
+CREATE TABLE products(
+    product_id SERIAL PRIMARY KEY,
+    product_type VARCHAR(50),
+    price FLOAT,
+    brand VARCHAR(50),
+    name VARCHAR(50),
+    image VARCHAR(300)
+);
+
+CREATE TABLE cart_items(
+    cart_id SERIAL PRIMARY KEY,
+    product_id SMALLINT
+);
